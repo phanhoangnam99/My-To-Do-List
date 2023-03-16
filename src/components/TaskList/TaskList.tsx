5320aa9e06e3c11f3db747b448e03065abd1e737
NamPhan <101878161+phanhoangnam99@users.noreply.github.com>
2023-03-16 15:19:16 +0700

    init

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import { Todo } from '../../@types/todo.type'
+import styles from './taskList.module.scss'
+
+interface TaskListProps {
+  doneTaskList?: boolean
+  todos: Todo[]
+  handleDoneTodo: (id: string, done: boolean) => void
+  startEditTodo: (id: string) => void
+  deleteTodo: (id: string) => void
+}
+
+export default function TaskList(props: TaskListProps) {
+  const { doneTaskList, todos, handleDoneTodo, startEditTodo ,deleteTodo} = props
+
+  // const handleChangeCheckbox = (idTodo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   handleDoneTodo(idTodo, event.target.checked)
+  // }
+
+  return (
+    <div>
+      <h2 className={styles.title}>{doneTaskList ? 'Hoàn thành' : 'Chưa hoàn thành'}</h2>
+
+      <div className={styles.tasks}>
+        {todos.map((todo) => (
+          <div className={styles.task} key={todo.id}>
+            <input
+              type='checkbox'
+              className={styles.taskCheckbox}
+              checked={todo.done}
+              onChange={(event) => {
+                handleDoneTodo(todo.id, event.target.checked)
+              }}
+            />
+            <span className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ''}`}>{todo.name}</span>
+            <div className={styles.taskAction}>
+              <button className={styles.taskBtn} onClick={() => startEditTodo(todo.id)}>
+                🖊️
+              </button>
+              <button className={styles.taskBtn} onClick={() => deleteTodo(todo.id)}>
+                🗑️
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import { Todo } from '../../@types/todo.type'
+import TaskInput from '../TaskInput'
+import TaskList from '../TaskList'
+import styles from './todoList.module.scss'
+export default function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>(null)
+
+  const doneTodos = todos.filter((todo) => todo.done)
+
+  const notdoneTodos = todos.filter((todo) => !todo.done)
+
+  const addTodo = (name: string) => {
+    const todo: Todo = {
+      name,
+      done: false,
+      id: new Date().toISOString()
+    }
+    setTodos((prev) => [...prev, todo])
+  }
+
+  const handleDoneTodo = (id: string, done: boolean) => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done }
+        }
+        return todo
+      })
+    })
+  }
+
+  const startEditTodo = (id: string) => {
+    const findedTodo = todos.find((todo) => todo.id === id)
+    if (findedTodo) {
+      setCurrentTodo(findedTodo)
+    }
+  }
+
+  const editTodo = (name: string) => {
+    setCurrentTodo((prev) => {
+      if (prev) {
+        return { ...prev, name }
+      }
+      return null
+    })
+  }
+
+  const finishEditTodo = () => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === (currentTodo as Todo).id) {
+          return currentTodo as Todo
+        }
+        return todo
+      })
+    })
+    setCurrentTodo(null)
+  }
+
+  const deleteTodo = (id: string) => {
+    if (currentTodo) {
+      setCurrentTodo(null)
+     
+    }
+    setTodos((prev) => {
+      const findIndexTodo = prev.findIndex((todo) => todo.id === id)
+      if (findIndexTodo > -1) {
+        const result = [...prev]
+        result.splice(findIndexTodo, 1)
+        return result
+      }
+      return prev
+    })
+  }
+
+  return (
+    <div className={styles.todoList}>
+      <div className={styles.todoListContainer}>
+        <TaskInput addTodo={addTodo} currentTodo={currentTodo} editTodo={editTodo} finishEditTodo={finishEditTodo} />
+        <TaskList
+          todos={notdoneTodos}
+          handleDoneTodo={handleDoneTodo}
+          startEditTodo={startEditTodo}
+          deleteTodo={deleteTodo}
+        />
+        <TaskList
+          doneTaskList
+          todos={doneTodos}
+          handleDoneTodo={handleDoneTodo}
+          startEditTodo={startEditTodo}
+          deleteTodo={deleteTodo}
+        />
+      </div>
+    </div>
+  )
+}
